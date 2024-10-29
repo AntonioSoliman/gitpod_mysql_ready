b45f9ebce6df483d38c401e81eafc3d83a207c0b
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-10-29 09:34:14 +0000

    completamento del main 2 python

---
+++ main_2.py
@@ -0,0 +1,13 @@
+#Craezione di una tabella con i vari elementi 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="mydatabase1"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
